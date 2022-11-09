ef04e7e0885fc5887ab1a18b324bf7b3138c5fba
Vadim Danilov <99761501+vadimdanilov1@users.noreply.github.com>
2022-11-09 15:31:21 +0400

    Update Main.java

--- src/ru/mail/vsdanilov/Main.java
+++ src/ru/mail/vsdanilov/Main.java
@@ -3,7 +3,7 @@ package ru.mail.vsdanilov;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        Game game = new Game();
+        Game game = new Game(0, 0, true);
         game.startGame();
 
     }
